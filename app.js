71af90abb2275ac8f0e314bdc0c7fc7cca960a1b
MayankRI <107388464+MayankRI@users.noreply.github.com>
2022-07-21 15:53:11 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,3 +1,15 @@
+import { OnePasswordConnect, ItemBuilder } from "@1password/connect";
+
+// Create new connector with HTTP Pooling
+const op = OnePasswordConnect({
+	serverURL: "0.0.0.0:8080",
+	token: "eyJhbGciOiJFUzI1NiIsImtpZCI6ImlzZzVrajQydmczaGN0emFnZXlqNG93ZzNlIiwidHlwIjoiSldUIn0.eyIxcGFzc3dvcmQuY29tL2F1dWlkIjoiRE5ISURMNUFLUkNZSkNBQlU3T1BHVjVPTVUiLCIxcGFzc3dvcmQuY29tL3Rva2VuIjoiWkhwYWtxVlZBOERWSkhpQmlnOFA3TDJFbmtjNXdQemQiLCIxcGFzc3dvcmQuY29tL2Z0cyI6WyJ2YXVsdGFjY2VzcyJdLCIxcGFzc3dvcmQuY29tL3Z0cyI6W3sidSI6IjV0eWx3Z3N2bHNoN2NwZjZtZW93cnp3NGttIiwiYSI6NDh9XSwiYXVkIjpbImNvbS4xcGFzc3dvcmQuY29ubmVjdCJdLCJzdWIiOiJJRllZU1RSTDY1QU5GREZRNERIVlBPVE9GRSIsImlhdCI6MTY1ODM5ODYwOCwiaXNzIjoiY29tLjFwYXNzd29yZC5iNSIsImp0aSI6IndrN2d6bTJ5ZDZob2l1ZmZoYm1ob25vaGdxIn0.E5HwbJ1qxLOF44bjN1O1vYddPUac66uSbN345fo4neIJ3y0DDHbzvMzkqGSMA3m_pBxGZ62PsQxIh9dVIkgQ6w",
+	keepAlive: true,
+});
+
+
+
+
 // import Express and routes
 import express from 'express';
 import routes from './source/routes/route.js'
